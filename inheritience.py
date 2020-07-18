630c9e6ccafad18bb015137086d32a3353b6ab93
isrt09 <mazedur@gmail.com>
2020-07-18 20:22:30 +0600

    Inheritence is created...

---
+++ inheritience.py
@@ -0,0 +1,18 @@
+import datetime
+class Book:
+    def __init__(self,name, author):
+        self.name   = name
+        self.author = author
+    def getInfo(self):
+        print('Book Name :',self.name)
+        print('Author Name :',self.author)
+
+class Publisher(Book):
+    publication = 'MacMillan'
+    def publishDate(self):
+        print('Published Date :',datetime.date.today())
+
+publish = Publisher('Harry Porter','J.K. Rowling')
+publish.getInfo()
+publish.publishDate()
+print('Book Publication :',publish.publication)
