d4a11be266fed30b1552f9d38880eef71979ac95
isrt09 <mazedur@gmail.com>
2020-07-19 03:00:46 +0600

    Encapsulation is added...

---
+++ encapsulation.py
@@ -0,0 +1,12 @@
+
+# Simple Encapsulation Mechanism in Python
+class Father:
+    def __init__(self,account):
+        self.account = account
+        self._account = account
+        self.__account = account
+
+obj = Father('FDR')
+print('Public Account is ready',obj.account)
+print('Protected Account is ready',obj._account)
+print('Private Account is ready',obj._Father__account)
